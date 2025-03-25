9c619450e7722056564f907fdf8b7262be75b998
ingridfsantos04 <ingrid.ferreira.santos04@gmail.com>
2025-03-25 20:35:36 -0300

    exercicio

---
+++ Udemy/semana.java
@@ -0,0 +1,39 @@
+package Udemy;
+
+import java.util.Scanner;
+
+public class semana {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        String dia;
+
+        if (x == 1) {
+            dia = "Domingo";
+        }
+        else if (x == 2) {
+            dia = "Segunda";
+        }
+        else if (x == 3) {
+            dia = "Terça";
+        }
+        else if (x == 4) {
+            dia = "Quarta";
+        }
+        else if (x == 5) {
+            dia = "Quinta";
+        }
+        else if (x == 6) {
+            dia = "Sexta";
+        }
+        else if (x == 7) {
+            dia = "Sábado";
+        }
+        else {
+            dia = "Valor invalido";
+        }
+    
+        sc.close();
+    }
+}
