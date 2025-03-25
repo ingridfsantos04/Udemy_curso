a8fd203566568e28a5c6eec00129086e755fafaf
ingridfsantos04 <ingrid.ferreira.santos04@gmail.com>
2025-03-25 19:17:55 -0300

    exercicio

---
+++ Udemy/nota.java
@@ -0,0 +1,25 @@
+package Udemy;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class nota {
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Primeiro Semestre: ");
+        double primeiro = sc.nextDouble();
+
+        System.out.println("Segundo Semestre: ");
+        double segundo = sc.nextDouble();
+
+        double anual = primeiro + segundo;
+        System.out.println("Nota final: " + anual);
+
+
+        sc.close();
+
+    }
+}
